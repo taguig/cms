96c7df6ea381d676ff89a4764a6efd82f20ec38c
taguig <taguigbt@live.fr>
2021-08-10 00:31:18 +0200

    http00

---
+++ http.php
@@ -0,0 +1,72 @@
+<?php 
+class http {
+
+private $param = [];
+private $instance=null;
+private $ExtentionImage="png|jpg|jpeg|gif";
+private $ExtentionDoc="doc|docs|pdf|xls";
+private $adressePage = [];
+private $contentTypeImage = [ 
+        "png" => "image/png",
+        "jpg"=>"image/jpeg"
+    ];
+    private $contentTypeDoc = [ 
+        "doc" => "",
+        "docs"=>"" ,
+        "pdf"=>"",
+        "xls"=>""
+    ];
+
+   public static function getInstance()
+    {
+        if (empty(self::$instance)) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+  public function setParam($name, $value)
+    {
+        $this->param[$name] = $value;
+    }
+     public function ExtracteData()
+    {
+        $data = explode("/", $_SERVER["REQUEST_URI"]);
+        array_map(function ($a) {
+            if (stripos($a, "-") != false && stripos($a, ".") == false) {
+                $tempdata = explode("-", $a);
+                $this->setParam($tempdata[0], $tempdata[1]);
+            } else if (!empty($a)) {
+                $this->setAdressePage($a);
+            }
+        }, $data);
+    }
+
+       private function setAdressePage($val)
+    {
+        $this->adressePage[] = $val;
+    }
+
+    public function getAdressePage($i)
+    {
+        return $this->adressePage[$i];
+    }
+
+    public function GetValue($key)
+    {
+        if (isset($_GET[$key]) && ! empty($_GET[$key])) {
+            return $_GET[$key];
+        }
+        return false;
+    }
+
+    public function PostValue($key)
+    {
+        if (isset($_POST[$key]) && ! empty($_POST[$key])) {
+            return $_POST[$key];
+        }
+        return false;
+    }
+}
+
+
+?>
