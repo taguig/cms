f16d5b602f19c3c1783ffc911ef7a0f9d2af901c
taguig <taguigbt@live.fr>
2021-08-20 14:59:18 +0200

    view002

--- view/composion.php
+++ view/composion.php
@@ -10,11 +10,8 @@
            self::$file[$name]=file_get_contents("/view/composion/".$name."composion");
           }
           $code=self::$file[$name];
-             if (isset($data) && is_array($data)){
-           foreach($data as $key=>$value){
-          $code=preg_replace("\{([\s]+)?".$key."([\s]+)?\}", $value,$code);
-           }
-         }
+           self::ConvertPermision($code,$data);
+           self::ConvertValue($code,$data);
          return $code;
           
         }
@@ -23,10 +20,38 @@
 
      public static function getComposonCode(&$code,$data=null):string{
        $out=[];
-       preg_match_all("\{%([\s]+)?composion ([\w]+) ([\w]+)?([\s]+)?%\}",$code,$out,PREG_SET_ORDER);
+       preg_match_all("\{%composion ([\w]+) ([\w]+)? %\}",$code,$out,PREG_SET_ORDER);
       foreach($out as $value){
         $code=str_replace($val[0],self::getComposion($val[1],(isset($data[$val[2]]))?$data[$val[2]]:""),$code);
       }
 
+     }
+     private static function ConvertValue(&$code,$data){
+  if (isset($data) && is_array($data)){
+           foreach($data as $key=>$value){
+          $code=preg_replace("\{%".$key."%\}", $value,$code);
+           }
+         }
+     }
+     private static function ConvertPermision (&$code ,$data){
+      $out=[];
+      $permission=http::getInstance()->getTypeUser();
+       preg_match_all("\{%user:(a|v|u)%\}((.|\n)*?)\{%enduser%\}",$code,$out,PREG_SET_ORDER);
+       foreach($out as $o){
+       if ($o[1]=='v'&& $o[1]==$permission ){
+          $code=preg_replace("\{%user:v%\}((.|\n)*?)\{%enduser%\}", $o[2],$code);
+          $code=preg_replace("\{%user:a%\}((.|\n)*?)\{%enduser%\}", "",$code);
+          $code=preg_replace("\{%user:u%\}((.|\n)*?)\{%enduser%\}", "",$code);
+       }else if($o[1]=='a' && $o[1]==$permission ){
+           $code=preg_replace("\{%user:a%\}((.|\n)*?)\{%enduser%\}", $o[2],$code);
+          $code=preg_replace("\{%user:v%\}((.|\n)*?)\{%enduser%\}", "",$code);
+          $code=preg_replace("\{%user:u%\}((.|\n)*?)\{%enduser%\}", "",$code);
+       }else if($o[1]=='u'&& $o[1]==$permission ){
+          $code=preg_replace("\{%user:u%\}((.|\n)*?)\{%enduser%\}", $o[2],$code);
+          $code=preg_replace("\{%user:v%\}((.|\n)*?)\{%enduser%\}", "",$code);
+          $code=preg_replace("\{%user:a%\}((.|\n)*?)\{%enduser%\}", "",$code);
+
+       }
+        }
      }
  }
--- view/view.php
+++ view/view.php
@@ -11,6 +11,7 @@ class view {
         $code="";
         if(file_exists("/view/pageview/".$this->name.".view")){
            $code=file_get_contents("/view/pageview/".$this->name.".view");
+           $this->ConvertPermision($code);
            $this->ConvertValue($code);
            $this->ConvertComposion($code);
         }else {
@@ -22,10 +23,31 @@ class view {
     }
     private function ConvertValue(&$code){
   foreach($this->data as $key->$value ){
-           $code=preg_replace("\{([\s]+)?".$key."([\s]+)?\}", $value,$code);  
+           $code=preg_replace("\{% ".$key." %\}", $value,$code);  
            }
     }
     private function ConvertComposion(&$code){
         composion::getComposonCode($code,$this->data);
     }
+       private  function ConvertPermision (&$code){
+      $out=[];
+      $permission=http::getInstance()->getTypeUser();
+       preg_match_all("\{%user:(a|v|u)%\}((.|\n)*?)\{%enduser%\}",$code,$out,PREG_SET_ORDER);
+       foreach($out as $o){
+       if ($o[1]=='v'&& $o[1]==$permission ){
+          $code=preg_replace("\{%user:v%\}((.|\n)*?)\{%enduser%\}", $o[2],$code);
+          $code=preg_replace("\{%user:a%\}((.|\n)*?)\{%enduser%\}", "",$code);
+          $code=preg_replace("\{%user:u%\}((.|\n)*?)\{%enduser%\}", "",$code);
+       }else if($o[1]=='a' && $o[1]==$permission ){
+           $code=preg_replace("\{%user:a%\}((.|\n)*?)\{%enduser%\}", $o[2],$code);
+          $code=preg_replace("\{%user:v%\}((.|\n)*?)\{%enduser%\}", "",$code);
+          $code=preg_replace("\{%user:u%\}((.|\n)*?)\{%enduser%\}", "",$code);
+       }else if($o[1]=='u'&& $o[1]==$permission ){
+          $code=preg_replace("\{%user:u%\}((.|\n)*?)\{%enduser%\}", $o[2],$code);
+          $code=preg_replace("\{%user:v%\}((.|\n)*?)\{%enduser%\}", "",$code);
+          $code=preg_replace("\{%user:a%\}((.|\n)*?)\{%enduser%\}", "",$code);
+
+       }
+        }
+     }
 }
